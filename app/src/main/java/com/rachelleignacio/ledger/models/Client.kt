11067b152e252b546e2ada3453ee21dd4868c049
rachelleignacio <ignacio.chelle88@gmail.com>
2018-09-08 17:04:29 -0700

    initial

---
+++ app/src/main/java/com/rachelleignacio/ledger/models/Client.kt
@@ -0,0 +1,6 @@
+package com.rachelleignacio.ledger.models
+
+data class Client(val id: String,
+                  val name: String,
+                  val ratePerHorse: Double,
+                  val ratePerHour: Double)
